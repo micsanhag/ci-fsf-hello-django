39fcc7aab422698b588208c313c3fa0b68667f88
michael santana <emacen@gmail.com>
2023-12-12 13:28:30 +0000

    update

--- todo/models.py
+++ todo/models.py
@@ -5,5 +5,5 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
